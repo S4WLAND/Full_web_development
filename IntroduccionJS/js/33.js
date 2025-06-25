fef2a2ca54e892d191180c51c7257dd81ae259a4
S4WLAND <stifhlife@gmail.com>
2025-06-25 00:58:39 -0300

    feat: JS introduction completed

--- IntroduccionJS/js/33.js
+++ IntroduccionJS/js/33.js
@@ -2,14 +2,19 @@ async function obtenerEmpleados() {
 
     const archivo = 'empleados.json';
 
-    // fetch(archivo)
-    //     .then( resultado => resultado.json())
-    //     .then( datos => {
-    //         // console.log(datos.empleados);
+    fetch(archivo)
+        .then( resultado => resultado.json())
+        .then( datos => {
+            // console.log(datos.empleados);
 
-    //         const { empleados } = datos;
-    //        console.log(empleados);
-    //     });
+            const {empleados} = datos;
+           console.log(empleados);
+
+            empleados.forEach(empleado => {
+                const {id, nombre, puesto} = empleado;
+                console.log(id, nombre, puesto);
+            });
+        });
 
     const resultado = await fetch(archivo);
     const datos = await resultado.json();
--- IntroduccionJS/js/todo.js
+++ IntroduccionJS/js/todo.js
@@ -74,7 +74,7 @@ result_math = Math.PI; // 3.141592653589793
 result_math = Math.round(3.14); // 3
 result_math = Math.ceil(3.14); // 4
 result_math = Math.floor(3.14); // 3
-result_math = Math.random(); // ramddom number between 0 and 1
+result_math = Math.random(); // random number between 0 and 1
 result_math = Math.floor(Math.random() * 100); // entre 0 y 99
 
 // operation orders
@@ -233,3 +233,137 @@ const user = {
 user.sayHello();
 
 
+// objet literal and constructor
+
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+}
+
+const product_constructor1 = new Product('gamer headphones', 100);
+const product_constructor2 = new Product('mechanical keyboard', 200);
+
+console.log(product_constructor1);
+console.log(product_constructor2);
+
+// class heritage and polymorphism
+class Product2 {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    sayHello() {
+        console.log(`Hello ${this.name} and the cost is ${this.price}`);
+    }
+
+
+}
+
+const product_class1 = new Product2('gamer headphones', 100);
+const product_class2 = new Product2('mechanical keyboard', 200);
+
+product_class1.sayHello();
+product_class2.sayHello();
+
+class Product3 extends Product2 {
+    constructor(name, price, discount) {
+        super(name, price);
+        this.discount = discount;
+    }
+    sayHello() {
+        console.log(`Hello ${this.name} and the cost is ${this.price} and the discount is ${this.discount}`);
+    }
+}
+
+const product_class3 = new Product3('gamer headphones', 100, 50);
+product_class3.sayHello();
+
+// try catch
+
+try {
+    console.log(numero2);
+} catch (error) {
+    console.log(error.name, ':', error.message);
+}
+
+product_class1.sayHello();
+
+
+// promises
+
+const userAuthenticated = () => new Promise( (resolve, reject) => {
+    const auth = false;
+
+    if(auth){
+        resolve('Usuario Autenticado');
+
+    } else {
+        reject('No se pudo iniciar sesión');
+    }
+});
+
+userAuthenticated()
+    .then( resultado => console.log(resultado))
+    .catch( error => console.log(error))
+
+
+
+const descargarArchivo = (nombre) => new Promise((resolve, reject) => {
+    console.log(`Iniciando descarga de ${nombre}...`);
+
+    setTimeout(() => {
+      const success = true; // simula si todo salió bien
+
+      if (success) {
+        resolve(`✅ Descarga completa: ${nombre}`);
+      } else {
+        reject(`❌ Error al descargar ${nombre}`);
+      }
+    }, 2000); // simula 2 segundos de espera
+  });
+
+// Usando la Promise:
+descargarArchivo("reporte.pdf")
+  .then(mensaje => {
+    console.log(mensaje); // Se muestra si todo salió bien
+  })
+  .catch(error => {
+    console.error(error); // Se muestra si hubo un error
+  });
+
+// Async / await
+
+function download_clients() {
+    return new Promise((resolve, reject) => {
+        console.log('Downloading clients...');
+        setTimeout(() => {
+            resolve('Clients downloaded');
+        }, 5000);
+    });
+}
+
+
+function download_orders() {
+    return new Promise((resolve, reject) => {
+        console.log('Downloading orders...');
+        setTimeout(() => {
+            resolve('Orders downloaded');
+        }, 3000);
+    });
+}
+
+async function app() {
+    try {
+        // const clients = await download_clients();
+        // const orders = await download_orders();
+        // console.log(clients);
+
+        const result = await Promise.all([download_clients(), download_orders()]);
+        console.log(result[0]);
+        console.log(result[1]);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app();
