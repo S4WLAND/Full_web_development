e6b54ace1e5ed70667d872046af5c2e8dd2eeefa
S4WLAND <stifhlife@gmail.com>
2025-06-30 01:53:30 -0300

    feat: Fix css compiler

--- Music_festival/gulpfile.js
+++ Music_festival/gulpfile.js
@@ -1,27 +1,30 @@
-import { src, dest, watch, series } from 'gulp'
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass'
+import { src, dest, watch, series } from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import terser from 'gulp-terser';
 
-const sass = gulpSass(dartSass)
+// Configure gulp-sass with the Dart Sass compiler
+const sass = gulpSass(dartSass);
 
-export function js( done ) {
-    src('src/js/app.js')
-        .pipe( dest('build/js') ) 
-
-    done()
+export function js() {
+  return src('src/js/app.js')
+    .pipe(terser())
+    .pipe(dest('build/js'));
 }
 
-export function css( done ) {
-    src('src/scss/app.scss', {sourcemaps: true})
-        .pipe( sass().on('error', sass.logError) )
-        .pipe( dest('build/css', {sourcemaps: '.'}) )
-
-    done()
+export function css() {
+  return src('src/scss/app.scss', { sourcemaps: true })
+    .pipe(
+      sass({ 
+        outputStyle: 'compressed'
+      }).on('error', sass.logError)
+    )
+    .pipe(dest('build/css', { sourcemaps: '.' }));
 }
 
 export function dev() {
-    watch('src/scss/**/*.scss', css)
-    watch('src/js/**/*.js', js)
+  watch('src/scss/**/*.scss', css);
+  watch('src/js/**/*.js', js);
 }
 
-export default series( js, css, dev )
+export default series(js, css, dev);
