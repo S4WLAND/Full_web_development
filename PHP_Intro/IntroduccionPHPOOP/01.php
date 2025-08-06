51eae340b3f8af0e2dda6a443c01ed1f6c83c2e6
S4WLAND <stifhlife@gmail.com>
2025-08-06 01:40:34 -0300

    feat: Class properties in php

--- PHP_Intro/IntroduccionPHPOOP/01.php
+++ PHP_Intro/IntroduccionPHPOOP/01.php
@@ -1,4 +1,32 @@
 <?php include 'includes/header.php';
 
+// definir una clase
+
+class Producto {
+
+    // propiedades o atributos
+    public $nombre;
+    public $precio;
+    public $disponible;
+
+
+}
+
+$producto = new Producto();
+
+$producto->nombre = 'Tablet';
+$producto->precio = 200;
+$producto->disponible = true;
+echo '<pre>';
+var_dump($producto);
+echo '</pre>';
+
+$producto2 = new Producto();
+$producto2->nombre = 'Monitor Curvo de 49"';
+$producto2->precio = 300;
+$producto2->disponible = true;
+echo '<pre>';
+var_dump($producto2);
+echo '</pre>';
 
 include 'includes/footer.php';
