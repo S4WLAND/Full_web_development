c78d1914bbcbfa1f1aa0cb582a36bbe547b8c0ff
S4WLAND <stifhlife@gmail.com>
2025-08-07 17:47:07 -0300

    feat: PDO object for database connection

--- PHP_Intro/IntroduccionPHPOOP/09.php
+++ PHP_Intro/IntroduccionPHPOOP/09.php
@@ -1,4 +1,21 @@
 <?php include 'includes/header.php';
 
+// conectar a la bd con mysqli
+
+$query = "SELECT titulo, imagen FROM propiedades";
+
+// preparando el query
+$stmt = $db->prepare($query);
+
+// ejecucion del query preparado
+$stmt->execute();
+
+// creación de la variable
+$stmt->bind_result($titulo, $imagen);
+
+// impresión del resultado
+while($stmt->fetch()):
+    var_dump($titulo);
+endwhile;
 
 include 'includes/footer.php';
--- PHP_Intro/IntroduccionPHPOOP/10.php
+++ PHP_Intro/IntroduccionPHPOOP/10.php
@@ -1,4 +1,21 @@
 <?php include 'includes/header.php';
 
+// conectando a la db con PDO
+$db = new PDO(
+    'mysql:host=localhost; dbname=bienes_raices_crud;charset=utf8',
+    'root', 
+    'root');
+
+$query = "SELECT titulo, imagen FROM propiedades";
+
+$stmt = $db->prepare($query);
+
+$stmt->execute();
+$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($resultado as $propiedad) {
+    echo $propiedad['titulo'] . '<br>';
+    echo $propiedad['imagen'] . '<br>';
+}
 
 include 'includes/footer.php';
